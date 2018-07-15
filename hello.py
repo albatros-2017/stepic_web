ff42867ec560b25002b67c6e4d6b8a05ba4d69b1
Yuliia Lymarenko <lymarenko@yandex.ru>
2018-07-15 18:41:41 +0300

    13 commit

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
 def app(environ, start_response):
-    # бизнес-логика
-    status = '200 OK'
-    headers = [
-    ('Content-Type', 'text/plain')
-    ]
-    body = bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),encoding="utf8")
-    start_response(status, headers )
-    return [ body ]
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),encoding="utf8")]
